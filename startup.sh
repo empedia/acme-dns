967d9b36e16133e173395d8ddca006a22baa84a9
empedia <empedia@gmail.com>
2025-06-01 10:42:41 +0000

    added back reuseaddr

--- startup.sh
+++ startup.sh
@@ -8,23 +8,24 @@ echo "--------------------------------------------------"
 # Wait a moment for acme-dns to start listening on 127.0.0.1:53
 sleep 2
 
-echo "--- Port 53 Listeners AFTER ACME DNS BEFORE SOCAT TCP---"
-ss -tulnp | grep ':53'
-echo "--------------------------------------------------"
-
-# Start socat for TCP forwarding (IPv6 only via [::])
-# Listen on [::]:53 (TCP) and forward to 127.0.0.1:53 (TCP)
-socat -6 -d -d TCP-LISTEN:53,fork,bind=[::] TCP:127.0.0.1:53 &
-
-sleep 2
-
-echo "--- Port 53 Listeners AFTER TCP SOCAT BEFORE UDP SOCAT---"
+echo "--- Port 53 Listeners BEFORE UDP SOCAT---"
 ss -tulnp | grep ':53'
 echo "--------------------------------------------------"
 
 # Start socat for UDP forwarding (IPv4 only via fly-global-services)
 # Listen on fly-global-services:53 (UDP) and forward to 127.0.0.1:53 (UDP)
 socat UDP-LISTEN:53,fork,bind=fly-global-services UDP:127.0.0.1:53 &
+
+
+echo "--- Port 53 Listeners AFTER UDP SOCAT BEFORE SOCAT TCP---"
+ss -tulnp | grep ':53'
+echo "--------------------------------------------------"
+
+# Start socat for TCP forwarding (IPv6 only via [::])
+# Listen on [::]:53 (TCP) and forward to 127.0.0.1:53 (TCP)
+socat -6 -d -d TCP-LISTEN:53,fork,bind=[::],reuseaddr TCP:127.0.0.1:53 &
+
+sleep 2
 
 echo "--- Port 53 Listeners AFTER TCP SOCAT ---"
 ss -tulnp | grep ':53'
